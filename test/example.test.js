fba29f458f259e17389e9915335d24c40fc11f4b
Leonardo Silva <42047532+leoesilva@users.noreply.github.com>
2025-05-24 08:48:30 -0300

    first commit

---
+++ test/example.test.js
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('Exemplo de Teste', function() {
+    it('Deve somar dois números corretamente', function() {
+        assert.strictEqual(1 + 1, 2);
+    });
+});
